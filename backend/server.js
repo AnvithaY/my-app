18ad6ec940bd609927a9a5240770f3285e61c7a0
AnvithaY <58981889+AnvithaY@users.noreply.github.com>
2020-03-15 20:51:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -68,6 +68,55 @@ app.post('/del',(req,res)=>{
 // })
 
 
+app.post('/login', async (req, res) => {
+   
+
+    var user = req.body.email;
+    var pass = req.body.password;
+ 
+ var q="select * from UserTable where email='"+user+"' and Password='"+pass+"' and userType='N'"
+ console.log(q)
+  
+       con.query(q, function (err, result, fields) {
+     if (err) console.log(err)
+         if(result.length == 1)
+         {
+             var abc={
+                 error:'',
+                 response:'success'
+             }
+         }
+         else
+         {
+             var abc={
+                 error:'No Such User Exists. Please Register first',
+                 response:'fail'
+             }
+ 
+         }
+         res.json(JSON.stringify(abc));
+ 
+   });
+     
+ });
+
+app.post('/',(req,res)=>{
+	var {name,rollno} =req.body;
+	var records = [[req.body.name,req.body.rollno]];
+	if(records[0][0]!=null)
+	{
+		con.query("INSERT into student (name,rollno) VALUES ?",[records],function(err,res,fields){
+
+			if(err) throw err;
+
+			console.log(res);
+		});
+	}
+	res.json('Form recieved');
+
+
+})
+
 app.post('/modify',(req,res)=>{
     // console.log("HERE")
     // var {fullName,eventfullName,email,location,date,hrs,choicedept} =req.body;
@@ -80,6 +129,49 @@ app.post('/modify',(req,res)=>{
     if(records[0][0]!=null)
 	{
 		con.query(sql,[records],function(err,resp,fields){
+
+            var abc={
+                error:err,
+                result:resp
+            }
+
+			if(err) console.log(err);
+
+            console.log(resp);
+            res.json(JSON.stringify(abc));
+		});
+    }
+    // console.log("Finished insert")
+    else
+    {
+        var abc={
+            error:"No record"
+        }
+        res.json(JSON.stringify(abc));
+    }
+    
+    	
+})
+
+// Abhishek:this.state.Abhishek,
+// Anvitha:this.state.Anvitha,
+// Asritha:this.state.Asritha,
+// Nithesh:this.state.Nithesh,
+// Vamsi:this.state.Vamsi,
+// Aditi:this.state.Aditi,
+// Thushara:this.state.Thushara,
+// Nayana:this.state.Nayana,
+// Sayannah:this.state.Sayannah,
+// Gayathri:this.state.Gayathri
+
+app.post('/attendance',(req,res)=>{
+    console.log("HERE")
+    // console.log(req.body.fullName)
+	var records = [[req.body.Abhishek,req.body.Anvitha,req.body.Asritha]];
+    
+    if(records[0][0]!=null)
+	{
+		con.query("INSERT into attendance (Abhishek,Anvitha,Asritha) VALUES ?",[records],function(err,resp,fields){
 
             var abc={
                 error:err,
